dad1de6850bf8b486d677a37618a47ae9e1d1b37
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-10-28 17:30:00 +0100

    added a new BachelorProgramme db to project

--- src/main/java/com/example/portfolio2/DatabaseConnection.java
+++ src/main/java/com/example/portfolio2/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:sqlite:";
+    private static final String URL = "jdbc:sqlite:identifier.sqlite";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
